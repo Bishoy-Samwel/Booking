a79ac2a8d25cbbd59484eb2de62cd691e9b27a5a
Bishoy <bishoy.samwuel@gmail.com>
2022-03-10 22:20:45 +0200

    Fix duration calculation

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -16,7 +16,7 @@ class AvailabilitiesController < ApplicationController
   # POST /availabilities
   def create
     @availability = Availability.new(availability_params)
-    @availability.duration = @availability.end_time - @availability.start_time
+    @availability.duration = (@availability.end_time - @availability.start_time)/(60*60)
     if @availability.save
       render json: @availability, status: :created, location: @availability
     else
