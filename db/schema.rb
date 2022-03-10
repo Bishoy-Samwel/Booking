66a306a1a2dc2618b70c0488dea4b5dff1ede7cd
Bishoy <bishoy.samwuel@gmail.com>
2022-03-10 12:56:27 +0200

    Migrate availabilities, sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_10_085032) do
+ActiveRecord::Schema.define(version: 2022_03_10_103619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "availabilities", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.decimal "duration"
+    t.bigint "teacher_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["teacher_id"], name: "index_availabilities_on_teacher_id"
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.string "name"
+    t.decimal "duration"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.decimal "duration"
+    t.bigint "teacher_id", null: false
+    t.bigint "lesson_id", null: false
+    t.bigint "student_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["lesson_id"], name: "index_sessions_on_lesson_id"
+    t.index ["student_id"], name: "index_sessions_on_student_id"
+    t.index ["teacher_id"], name: "index_sessions_on_teacher_id"
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "name"
@@ -27,4 +58,8 @@ ActiveRecord::Schema.define(version: 2022_03_10_085032) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "availabilities", "teachers"
+  add_foreign_key "sessions", "lessons"
+  add_foreign_key "sessions", "students"
+  add_foreign_key "sessions", "teachers"
 end
