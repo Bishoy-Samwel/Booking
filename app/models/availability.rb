ee8d51e7a48581d66a6cd74f631ffe44a7ed01ee
Bishoy <bishoy.samwuel@gmail.com>
2022-03-10 22:03:40 +0200

    Add validation on dates of Availability

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,3 +1,14 @@
+class DatesValidator < ActiveModel::Validator
+  def validate(record)
+    if record.start_time >= record.end_time
+      record.errors.add :base, "end_time should be after start_time"
+    end
+  end
+end
+
 class Availability < ApplicationRecord
   belongs_to :teacher
+  validates :start_time, :end_time, uniqueness: true
+  validates_with DatesValidator
 end
+
