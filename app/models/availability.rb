ab66cd3a92036391f3468562cde3bbc263f1f7e5
Bishoy <bishoy.samwuel@gmail.com>
2022-03-10 22:21:23 +0200

    Add  validation of availability lower than 1 day

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -3,6 +3,9 @@ class DatesValidator < ActiveModel::Validator
     if record.start_time >= record.end_time
       record.errors.add :base, "end_time should be after start_time"
     end
+    if (record.end_time - record.start_time)/(60*60) > 24
+      record.errors.add :base, "Duration should be lower than 24 hours"
+    end
   end
 end
 
