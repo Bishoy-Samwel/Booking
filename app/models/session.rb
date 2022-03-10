6f99befb196a8de11854cd0d35ec5ea5b00c05f4
Bishoy <bishoy.samwuel@gmail.com>
2022-03-11 00:40:37 +0200

    Add availability validator for new sessions

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,5 +1,17 @@
+class AvailablilityValidator < ActiveModel::Validator
+  def validate(record)
+    lDuration = Lesson.find(record.lesson_id).duration
+
+    availability =  Availability.where("duration >= ?", lDuration)[0]
+    unless availability
+      record.errors.add :name, "Currently there is no available teacher, Try again later"
+    end
+  end
+end
+
 class Session < ApplicationRecord
   belongs_to :teacher
   belongs_to :lesson
   belongs_to :student
+  validates_with AvailablilityValidator
 end
